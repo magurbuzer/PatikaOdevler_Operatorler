2b1bbb7b78f689a37fda6d8ae05ac87a113235ca
magbits <magurbuzer@oaf.anadolu.edu.tr>
2022-10-29 18:29:41 +0300

    SeconCommit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PatikaOdevler_Operatorler
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
